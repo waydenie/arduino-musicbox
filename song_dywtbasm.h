e6b6ef6ac5b5e9f6752302f285ffd6cc94fcab7b
Wayde Nie <niew@mcmaster.ca>
2015-12-26 16:50:02 -0500

    fix note element number for dywtbasm, was 84, should have been 85.

--- song_dywtbasm.h
+++ song_dywtbasm.h
@@ -1,5 +1,5 @@
 const int song_dywtbasm[][2] PROGMEM = {
-  {84,0}, //Total Number of Notes
+  {85,0}, //Total Number of Notes
 
   {NOTE_REST,2},{NOTE_DS4,1}, {NOTE_DS4,1}, {NOTE_DS4,1},{NOTE_AS3,1},{NOTE_DS4,1},{NOTE_G4,1},              //Bar  1
   {NOTE_F4,2},  {NOTE_G4,6},                                                                                 //Bar  2
