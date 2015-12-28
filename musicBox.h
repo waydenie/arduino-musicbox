45f3bff50ef270999f2a9fe88fc492f19077fa75
Wayde Nie <niew@mcmaster.ca>
2015-12-27 23:58:41 -0500

    some cleanup after moving base note duration into element[0] in song_ array

--- musicBox.h
+++ musicBox.h
@@ -20,8 +20,7 @@ volatile int melodyNum   = 0;
 volatile int currentNote = 1;
 volatile int totalNotes  = 0;
 
-int   ms_per8th         = 160;
-int   ms_perNote        = 80;
+int   ms_perNote        = 160;
 float ms_interNotePause = 1.3;
 
 const int song_null[][2] PROGMEM = {
